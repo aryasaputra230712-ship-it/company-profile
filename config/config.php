8fdea8c62d633f82a1261228f4b3b524e4253475
Arya Dwi <aryasaputra2334@gmail.com>
2026-05-14 14:44:22 +0800

    konfigurasi database

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "db_company_profile";
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+if (!$conn) {
+    die("Tidak dapat akses database");
+}
