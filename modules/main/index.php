9123ba0894898255d7547ef2d71f0bb6afb91c05
Arya Dwi <aryasaputra2334@gmail.com>
2026-05-14 17:36:37 +0800

    tambahan file baru dan mengganti nama folder pages menjadi layouts

---
+++ modules/main/index.php
@@ -0,0 +1,36 @@
+<?php 
+define('ROOTPATH', $_SERVER['DOCUMENT_ROOTPATH']. '/db_company_profile');
+
+include ROOTPATH . "/config/config.php";
+include ROOTPATH . "/layouts/header.php";
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="">
+</head>
+
+<body>
+    <main>
+        <div class="container-main">
+            <div class="main-intro">
+                <video muted autoplay loop>
+                    <source src="../../assets/videos/mainVideo.mp4" type="video/mp4">
+                </video>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio, praesentium.</p>
+            </div>
+
+        </div>
+
+
+    </main>
+</body>
+
+</html>
